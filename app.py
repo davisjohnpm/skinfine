bb375c9500a9c411dbc21f5005135e6da9e9f9ca
davisjohnpm <77391526+davisjohnpm@users.noreply.github.com>
2021-12-16 04:55:15 -0600

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'JPG', 'JPEG', 'png'])
 app = Flask(__name__)
 
 def get_as_base64(url):
-    return base64.b64encode(requests.get(url, timeout=60).content)
+    return base64.b64encode(requests.get(url).content)
 ##### above used to be just url, no timeout
 
 def ensemble_predict(test_data):
